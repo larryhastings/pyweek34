e19f43e251702971e3a259b246fb6b6732fb105b
Daniel Pope <mauve@mauveweb.co.uk>
2022-09-11 00:58:50 +0100

    End screen, untested

--- main.py
+++ main.py
@@ -1891,6 +1891,27 @@ async def title_screen():
             ns.cancel()
 
 
+async def end_screen():
+    layer = scene.layers[hud_layer + 1]
+    layer.parallax = 0
+
+    with layer.add_label(
+        "Well done! You are a winner!",
+        font=FONT,
+        fontsize=48,
+        align="center",
+    ) as text:
+        async with w2d.Nursery() as ns:
+            ns.do(floating_wobble(text))
+            bg = scene.background
+            scene.background = '#56a9c4'
+            try:
+                await w2d.next_event(pygame.KEYDOWN, key=pygame.K_SPACE)
+            finally:
+                scene.background = bg
+            ns.cancel()
+
+
 START_LEVEL = 'tutorial_01'
 
 
@@ -1901,7 +1922,7 @@ async def level_progression(start_level: str = START_LEVEL):
         level = Level(level_name)
         await level.run()
         if not (level_name := level.next_level):
-            print("That's all the levels we have, thanks for playing!")
+            await end_screen()
             break
 
 
